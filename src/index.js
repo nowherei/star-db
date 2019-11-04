8687ef8aa50515001d037acf1411bcd5ef51552f
Dmitriy Prokoshev <prokoshev.da@yandex.ru>
2019-11-05 00:07:30 +0300

    Make components StarDB.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './componets/app';
 
-class SwapiService {
+// import SwapiService from "./services/swapi-service";
 
-  _apiBase = 'https://swapi.co/api';
+// const swapi = new SwapiService();
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` +
-        `, received ${res.status}`);
-    }
-  return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-  }
-
-  async getAllStarshops() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
-  }
-}
-
-const swapi = new SwapiService();
+ReactDOM.render(<App />,
+  document.getElementById('root'));
