e9e1becb511689308e8dcd0b2c544339992b64e9
Dmitriy Prokoshev <prokoshev.da@yandex.ru>
2019-11-04 22:38:06 +0300

    class SwapiService - для работы с swapi.co.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 
-const getResource = async (url) => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
+class SwapiService {
 
-getResource('https://swapi.co/api/people/1/')
-  .then((body) => {
-    console.log(body);
-  });
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` +
+        `, received ${res.status}`);
+    }
+  return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  }
+
+  async getAllStarshops() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  }
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  }
+}
+
+const swapi = new SwapiService();
