f50b1ed9aa4ce9f6bbe92c93f46f1a136ba7aba2
Dmitriy Prokoshev <prokoshev.da@yandex.ru>
2019-11-05 22:45:54 +0300

    Make Random Planet. Make transform data from API.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './componets/app';
 
-// import SwapiService from "./services/swapi-service";
-
-// const swapi = new SwapiService();
-
 ReactDOM.render(<App />,
   document.getElementById('root'));
