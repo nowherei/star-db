0f516b5e77436409a8575d742487bf447fcf2b84
Dmitriy Prokoshev <prokoshev.da@yandex.ru>
2019-11-30 01:11:43 +0300

    Props-Elements Add Element-container Row.

--- src/componets/people-page/people-page.js
+++ src/componets/people-page/people-page.js
@@ -5,6 +5,7 @@ import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import ErrorIndicator from "../error-idicator/error-indicator";
 import SwapiService from "../../services/swapi-service";
+import Row from '../row';
 
 export default class PeoplePage extends Component {
 
@@ -34,18 +35,20 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />
     }
 
+    const itemList = (
+      <ItemList
+        onItemSelected={this.onPersonSelected}
+        getData={this.swapiService.getAllPeople}
+        renderItem={({name, gender, birthYear}) => (
+          `${name} (${gender}, ${birthYear})`)} />
+    );
+
+    const personDetails = (
+      <PersonDetails personId={this.state.selectedPerson} />
+    );
+
     return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={({name, gender, birthYear}) => `${name} (${gender}, ${birthYear})`} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={this.state.selectedPerson} />
-        </div>
-      </div>
+      <Row left={itemList} right={personDetails} />
     );
   }
 }
