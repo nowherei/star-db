bcc0d27b5bdbc545036a9181c27af8503c27a8a9
Dmitriy Prokoshev <prokoshev.da@yandex.ru>
2019-12-01 00:42:02 +0300

    Refactoring PersonDetails to ItemDetails.

--- src/componets/people-page/people-page.js
+++ src/componets/people-page/people-page.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import './people-page.css';
 import ItemList from "../item-list";
-import PersonDetails from "../person-details";
+import ItemDetails from "../item-details";
 import SwapiService from "../../services/swapi-service";
 import Row from '../row';
 import ErrorBoundry from "../error-boundry";
@@ -37,7 +37,7 @@ export default class PeoplePage extends Component {
 
     const personDetails = (
       <ErrorBoundry>
-        <PersonDetails personId={this.state.selectedPerson} />
+        <ItemDetails itemId={this.state.selectedPerson} />
       </ErrorBoundry>
     );
 
