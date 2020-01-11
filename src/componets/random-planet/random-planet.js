64a5d304537e6f7444ee08b591673e5ec2ea035c
Dmitriy Prokoshev <prokoshev.da@yandex.ru>
2020-01-11 17:38:33 +0300

    propTypes

--- src/componets/random-planet/random-planet.js
+++ src/componets/random-planet/random-planet.js
@@ -10,7 +10,19 @@ export default class RandomPlanet extends Component {
 
   static defaultProps = {
     updateInterval: 10000
-  }
+  };
+
+  static propTypes = {
+    updateInterval: (props, propName, componentName) => {
+      const value = props[propName];
+
+      if (typeof value === 'number' && !isNaN(value)) {
+        return null;
+      }
+
+      return new TypeError(`${componentName}: ${propName} must be number`);
+    }
+  };
 
   swapiService = new SwapiService();
 
